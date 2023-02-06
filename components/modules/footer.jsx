756846450c731b738003bef285a943702c4fe54e
hendhyhutomo <hendhyhutomo@gmail.com>
2023-02-06 18:44:53 +0700

    Fix Hydration Error Related to Time

--- components/modules/footer.jsx
+++ components/modules/footer.jsx
@@ -1,40 +1,46 @@
-import { useEffect, useState } from 'react'
-import MailchimpSubscribe from 'react-mailchimp-subscribe'
-import SubscribeForm from './subscribe'
+import { useEffect, useState } from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import SubscribeForm from './subscribe';
 
-import Container from './container'
-import FancyLink from '../utils/fancyLink'
-import { transition } from '@/helpers/preset/tailwind'
-import { Youtube, Facebook, Instagram, Linkedin, Mail } from '@/helpers/preset/svg'
+import Container from './container';
+import FancyLink from '../utils/fancyLink';
+import { transition } from '@/helpers/preset/tailwind';
+import {
+  Youtube,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Mail,
+} from '@/helpers/preset/svg';
 
 const Footer = ({ className = '', footer, mailchimp = '' }) => {
-  const MAILCHIMP_URL = mailchimp
-  const [time, setTime] = useState(new Date())
+  const MAILCHIMP_URL = mailchimp;
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
-      setTime(new Date())
-    }, 5000)
+      setTime(new Date());
+    }, 5000);
 
     return () => {
-      clearInterval(timeInterval)
-    }
-  }, [])
+      clearInterval(timeInterval);
+    };
+  }, []);
 
   return (
     <footer
       className={`w-full h-auto max-md:h-full flex bg-offBlack text-white px-0 py-14 max-md:py-10 no-select-all ${className}`}
     >
-      <Container className="flex max-md:flex-col max-md:px-6">
-        <div className="h-full w-full max-md:w-full flex flex-col">
-          <div className="w-full mb-8">
-            <span className="text-sm">
+      <Container className='flex max-md:flex-col max-md:px-6'>
+        <div className='h-full w-full max-md:w-full flex flex-col'>
+          <div className='w-full mb-8'>
+            <span className='text-sm'>
               UBUD{' '}
-              {time.toLocaleTimeString([], {
+              {new Intl.DateTimeFormat('sv-SE', {
                 hour: '2-digit',
                 minute: '2-digit',
                 timeZone: 'Asia/Makassar',
-              })}
+              }).format(time)}
             </span>
           </div>
           {/* Subscription Form  */}
@@ -50,10 +56,9 @@ const Footer = ({ className = '', footer, mailchimp = '' }) => {
             )}
           />
         </div>
-        <div className="w-full h-full max-md:mt-10 flex flex-col justify-between relative -right-3 max-md:right-0 max-md:-left-3">
-          {
-            footer.show_socmed && 
-            <div className="w-auto h-full space-x-4 flex justify-end max-md:justify-start">
+        <div className='w-full h-full max-md:mt-10 flex flex-col justify-between relative -right-3 max-md:right-0 max-md:-left-3'>
+          {footer.show_socmed && (
+            <div className='w-auto h-full space-x-4 flex justify-end max-md:justify-start'>
               {footer.footerLink && footer.footerLink.email && (
                 <FancyLink
                   destination={`mailto:${footer.footerLink.email}`}
@@ -68,7 +73,7 @@ const Footer = ({ className = '', footer, mailchimp = '' }) => {
                   blank={true}
                   className={`relative w-10 h-10 setflex-center ${transition.fade}`}
                 >
-                  <Instagram fill={'#FFF'} className="w-4 h-4" />
+                  <Instagram fill={'#FFF'} className='w-4 h-4' />
                 </FancyLink>
               )}
               {footer.footerLink && footer.footerLink.facebook && (
@@ -77,7 +82,7 @@ const Footer = ({ className = '', footer, mailchimp = '' }) => {
                   blank={true}
                   className={`relative w-10 h-10 setflex-center ${transition.fade}`}
                 >
-                  <Facebook fill={'#FFF'} className="w-4 h-4" />
+                  <Facebook fill={'#FFF'} className='w-4 h-4' />
                 </FancyLink>
               )}
               {footer.footerLink && footer.footerLink.youtube && (
@@ -86,7 +91,7 @@ const Footer = ({ className = '', footer, mailchimp = '' }) => {
                   blank={true}
                   className={`relative w-10 h-10 setflex-center ${transition.fade}`}
                 >
-                  <Youtube fill={'#FFF'} className="w-4 h-4" />
+                  <Youtube fill={'#FFF'} className='w-4 h-4' />
                 </FancyLink>
               )}
               {footer.footerLink && footer.footerLink.linkedin && (
@@ -95,18 +100,18 @@ const Footer = ({ className = '', footer, mailchimp = '' }) => {
                   blank={true}
                   className={`relative w-10 h-10 setflex-center  ${transition.fade}`}
                 >
-                  <Linkedin fill={'#FFF'} className="w-4 h-4" />
+                  <Linkedin fill={'#FFF'} className='w-4 h-4' />
                 </FancyLink>
               )}
             </div>
-          }
-          <div className="w-full h-full max-md:mt-8 flex justify-end max-md:justify-start items-end max-md:items-start pr-3 max-md:pr-0 max-md:pl-3">
-            <span className="text-sm">{footer.creditText}</span>
+          )}
+          <div className='w-full h-full max-md:mt-8 flex justify-end max-md:justify-start items-end max-md:items-start pr-3 max-md:pr-0 max-md:pl-3'>
+            <span className='text-sm'>{footer.creditText}</span>
           </div>
         </div>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
