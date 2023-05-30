46d31d23720678c884c8069e659cc4f455489497
locavore-tomo <locavore@tomostudio.id>
2023-05-30 13:29:49 +0700

    💄 update features & facilities mobile layout

--- pages/nxt/features-facilities/detail.jsx
+++ pages/nxt/features-facilities/detail.jsx
@@ -48,10 +48,10 @@ const FeaturesAndFacilitiesDetail = ({ seoAPI, footerAPI }) => {
         variants={fade}
         className="no-select-all bg-black"
       >
-        <div className="relative w-full h-full flex mb-10">
-          <div className="absolute top-0 left-0 w-1/2 h-full">
+        <div className="relative w-full h-full flex flex-col md:flex-row mb-10">
+          <div className="relative md:absolute top-0 left-0 w-full md:w-1/2 h-full">
             <div className="w-full h-full flex flex-col">
-              <div className="relative w-full h-screen">
+              <div className="relative w-full h-[500px] md:h-screen">
                 <Image
                   src={feature1_big}
                   alt=""
@@ -59,7 +59,7 @@ const FeaturesAndFacilitiesDetail = ({ seoAPI, footerAPI }) => {
                   style={{ objectFit: 'cover' }}
                 />
               </div>
-              <div className="relative w-full h-screen">
+              <div className="relative w-full h-[500px] md:h-screen">
                 <Image
                   src={feature2_big}
                   alt=""
@@ -70,15 +70,15 @@ const FeaturesAndFacilitiesDetail = ({ seoAPI, footerAPI }) => {
             </div>
           </div>
           <Container className="relative w-full flex">
-            <div className="w-full h-full flex flex-col">
+            <div className="hidden w-full h-full md:flex flex-col">
               <div className="w-full h-screen" />
               <div className="w-full h-screen" />
             </div>
-            <div className="w-full h-screen sticky top-0 left-0 setflex-center">
-              <h1 className="text-d-additionalHeader m-0 text-[#BEC29D] font-funkturm">
+            <div className="w-full md:h-screen md:sticky top-0 left-0 setflex-center mt-16">
+              <h1 className="text-m-additionalHeader md:text-d-additionalHeader m-0 text-[#BEC29D] font-funkturm">
                 DISTILLERY
               </h1>
-              <div className="relative w-[165px] h-[165px] my-16">
+              <div className="relative w-[87px] md:w-[165px] h-[87px] md:h-[165px] my-10 md:my-16">
                 <Image
                   src={leaf}
                   alt=""
@@ -86,7 +86,7 @@ const FeaturesAndFacilitiesDetail = ({ seoAPI, footerAPI }) => {
                   style={{ objectFit: 'contain' }}
                 />
               </div>
-              <p className="text-center text-d-body text-white max-w-sm">
+              <p className="text-center text-m-body md:text-d-body text-white max-w-sm">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
